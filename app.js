2e973c3d121f5545289638b13fce5dd89aa64230
Tom Arnold <tarnold@kfh.co.uk>
2022-11-22 17:58:29 +0000

    App.js started

---
+++ app.js
@@ -0,0 +1,38 @@
+// Enters details on the form
+// Event listner
+// Run Book Constructor to create Book
+// 
+
+// Book Constuctor 
+
+class Book {
+    constructor(bookName, author, isbn){
+        
+        this.bookName = bookName; 
+        this.author = author; 
+        this.isbn = isbn ; 
+
+    }
+
+    storeBookLocalStorage(){
+        console.log("object");
+    }
+
+    displayBook(){
+        console.log("Display Book");
+    }
+
+    deleteBook(){
+        console.log("Delete Book from UI and ");
+    }
+
+    
+}
+
+// UI Elements 
+
+// Load Event Listner 
+
+// Contruct Book, add to UI and add to LS
+
+
