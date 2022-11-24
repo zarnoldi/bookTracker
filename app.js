3e50ced737dcde89caccf83e04fc1cff185b2c93
Tom Arnold <tarnold@kfh.co.uk>
2022-11-24 18:31:50 +0000

    added book display and delete book

--- app.js
+++ app.js
@@ -14,26 +14,28 @@ class Book {
 
     }
 
-    storeBookInLocalStorage() {
-        let books;
-        let book = this.book; 
-    
-        if (localStorage.getItem('books') === null) {
-            books = []; 
-        }else{
-            books = JSON.parse(localStorage.getItem('books'))
-        }
-    
-        books.push(book);
-        console.log(books);
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+    // storeBookLocalStorage(){
+    //     console.log(this.Book);
+    // }
 
     displayBook(){
-        console.log("Display Book");
+        let bookRow = document.createElement('tr'); 
+        bookRow.innerHTML = `
+        <th scope="row">${this.bookName}</th>
+        <th>${this.author}</th>
+        <th>${this.isbn}</th>
+        <th>
+        <a href="#" class="float-end delete-item">
+        <button type="button" class="btn-close" aria-label="Close"></button>
+        </a>
+        </th>
+        `;
+        tableBody.appendChild(bookRow);
+
     }
 
     deleteBook(){
+
         console.log("Delete Book from UI and ");
     }
 
@@ -48,22 +50,53 @@ const btnSubmit = document.getElementById('btnSubmit');
 const bookName = document.getElementById('bookName');
 const author = document.getElementById('author'); 
 const isbn = document.getElementById('isbn'); 
+const tableBody = document.getElementById('tableBody'); 
 
-// Load Event Listner 
-btnSubmit.addEventListener('click', (e)=>{
+// Load Event Listners
+loadEventListners(); 
 
-    let book = new Book(bookName.value, author.value, isbn.value)
-
-    book.storeBookInLocalStorage();
+// Add Book
+function loadEventListners() {
     
-    // console.log(localStorage);
+    // Add Book Event
+    btnSubmit.addEventListener('click', (e)=>{
+        // Construct Book
+        let book = new Book(bookName.value, author.value, isbn.value)
+        // Add Book to Local Storage
+        storeBookInLocalStorage(book);
+        // Display book in the UI 
+        book.displayBook(); 
+    
+        e.preventDefault(); 
+    })
 
-    e.preventDefault(); 
-   
-})
+    // Delete Book Event
+    tableBody.addEventListener('click', (e)=>{
 
-// Contruct Book, add to UI and add to LS
+        console.log(e.target);
+
+        if (e.target.parentElement.classList.contains('delete-item')) {
+            e.target.parentElement.parentElement.parentElement.remove(); 
+        }
+
+    })
+    
+
+}
+
 
 
 // Store book in local storage 
 
+function storeBookInLocalStorage(book) {
+    let books;
+
+    if (localStorage.getItem('books') === null) {
+        books = []; 
+    }else{
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+}
